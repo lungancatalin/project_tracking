9183a763abfd5d66adcf3f9d41d44df5f6037d87
Catalin <lungancatalin@yahoo.com>
2020-04-26 20:02:39 +0300

    Updates

---
+++ precache-manifest.1105124c957bd406578fbacad20c52bd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43eaf1471d1966475af6d4c2ddb4c12f",
+    "url": "/project_tracking/index.html"
+  },
+  {
+    "revision": "58bd4af8d72b60754781",
+    "url": "/project_tracking/static/css/main.fef066b4.chunk.css"
+  },
+  {
+    "revision": "13ceb36640522c1c86e7",
+    "url": "/project_tracking/static/js/2.70094262.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project_tracking/static/js/2.70094262.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58bd4af8d72b60754781",
+    "url": "/project_tracking/static/js/main.daa50c8a.chunk.js"
+  },
+  {
+    "revision": "e65a6149ebdb42ae2237",
+    "url": "/project_tracking/static/js/runtime-main.0f5ba882.js"
+  },
+  {
+    "revision": "f563a0c6761ae88a863d7eedcb46db83",
+    "url": "/project_tracking/static/media/icon-close.f563a0c6.svg"
+  },
+  {
+    "revision": "4648b31cc170be2c777fbc5367a2b49e",
+    "url": "/project_tracking/static/media/icon-hamburger.4648b31c.svg"
+  },
+  {
+    "revision": "d8da55ea482f7b38968af822c2fafe55",
+    "url": "/project_tracking/static/media/illustration-devices.d8da55ea.svg"
+  },
+  {
+    "revision": "dfc21d9af3253582a8ba91e911f5f7bb",
+    "url": "/project_tracking/static/media/logo.dfc21d9a.svg"
+  }
+]);
